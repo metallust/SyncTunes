48b3a968f96a8c7d2b79d5f9dec555ff756aa72a
Irfan <shaikh.irfan.oct16@gmail.com>
2023-06-18 12:22:33 +0530

    minor changes

--- sample_config.py
+++ sample_config.py
@@ -1,3 +1,3 @@
 client_id = "YOUR_CLIENT_ID"
-client_secret="YOUR_CLIENT_SECRET"
-redirect_uri="http://127.0.0.1:8080/"
+client_secret = "YOUR_CLIENT_SECRET"
+redirect_uri = "http://127.0.0.1:8080/"
